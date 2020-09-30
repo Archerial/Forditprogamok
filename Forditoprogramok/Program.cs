c39220f81be8b34b32dac56c486299168076dbbc
Archerial <agadam98@gmail.com>
2020-09-30 13:34:49 +0200

    Add 2nd lessons changes

--- Forditoprogramok/Program.cs
+++ Forditoprogramok/Program.cs
@@ -12,7 +12,8 @@ namespace filemanager
             sourceHandler s = new sourceHandler(@"C:\Users\Adam\Desktop\Forditoprogramok\Forditoprogramok\bin\Debug\file1.txt", "file2.txt");
             s.openFileToRead();
             Console.WriteLine(s.Content);
-            s.replaceContent("some kinda text");
+            s.replaceFirst();
+            //s.replaceContent(" some kinda text");
             Console.WriteLine(s.Content);
             s.openFileToWrite();
             Console.WriteLine("done");
--- Forditoprogramok/sourceHandler.cs
+++ Forditoprogramok/sourceHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Forditoprogramok
 {
@@ -9,7 +10,6 @@ namespace Forditoprogramok
     {
         private string path1, path2 = "";
         private string content = "";
-
 
         public string Path1
         {
@@ -28,7 +28,6 @@ namespace Forditoprogramok
             get { return this.content; }
             set { this.content = value; }
         }
-
 
         public sourceHandler(string path1, string path2)
         {
@@ -56,6 +55,61 @@ namespace Forditoprogramok
             }
         }
 
+        //replaceText("  "," ");
+        public void replaceText(string from, string to) {
+            while (content.Contains(from)) {
+                content = content.Replace(from, to);
+            }
+        }
+
+        public void replaceFirst(){
+
+            Dictionary<string,string> fromTo=new Dictionary<string,string>();
+            /*List<string> from = new List<string>();
+            List<string> to = new List<string>();*/
+
+
+            content = Regex.Replace(content,@"//(.*?)\r?\n","");
+            content = Regex.Replace(content, @"/\*(.*?)\*/","");
+            content = Regex.Replace(content, @"/\*[\w\W]*\*/","");
+
+            fromTo.Add("  "," ");
+            fromTo.Add("\r\n"," ");
+            fromTo.Add("    ", " ");    //Tab to 1 space
+            fromTo.Add(" {", "{");
+            fromTo.Add(" }", "}");
+            fromTo.Add("{ ", "{");
+            fromTo.Add("} ", "}");
+            fromTo.Add(" (", "(");
+            fromTo.Add(" )", ")");
+            fromTo.Add("( ", "(");
+            fromTo.Add(") ", ")");
+            fromTo.Add(" ;", ";");
+            fromTo.Add("; ", ";");
+            fromTo.Add(" =", "=");
+            fromTo.Add("= ", "=");
+
+
+            #region
+            /*
+            from.Add("  ");
+            to.Add(" ");
+            from.Add("\br");
+            to.Add(" ");
+
+            for (int i = 0; i < from.Count; i++)
+            {
+                replaceText(from[i], to[i]);
+            }*/
+            #endregion
+
+            foreach (KeyValuePair<string,string> kvp in fromTo)
+            {
+                replaceText(kvp.Key, kvp.Value);
+            }
+        }
+
+
         public void replaceContent(string s)
         {
             try
@@ -72,7 +126,7 @@ namespace Forditoprogramok
             try
             {
                 StreamWriter SW = new StreamWriter(File.Open(this.path2, FileMode.Create));
-                SW.WriteLine("some text");
+                SW.WriteLine(content);
                 SW.Flush();
                 SW.Close();
             }
